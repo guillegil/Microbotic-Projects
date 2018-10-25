37e0586aadf985c18663f685a9ab8c6f72a90481
Pedro Rico Pinazo <pedroricopinazo@gmail.com>
2018-10-25 14:36:08 +0200

    Add comments to config.h
    By: Pedro Rico Pinazo (pedroricopinazo@gmail.com)

--- config.h
+++ config.h
@@ -10,17 +10,17 @@
 
 #include "driverlib/pwm.h"
 
-#define MOTORS_FREQUENCY        50          // Frequency (Hz) of the servomotors
-//#define SERVOM_DUTYCYCLE        0.076       // Duty cycle of the servomotors
+#define MOTORS_FREQUENCY        50                  // Frequency (Hz) of the servomotors
+//#define SERVOM_DUTYCYCLE        0.076             // Duty cycle of the servomotors
 
-#define MAX_FORWARD_SPEED       1000
-#define MAX_BACKWARD_SPEED      -1000
+#define MAX_FORWARD_SPEED       1000                // Max value passed to MotorsQueue for forward movement
+#define MAX_BACKWARD_SPEED      -1000               // Max value passed to MotorsQueue for backward movement
 
-#define MAX_MOTORS_DUTYCYCLE        0.1f
-#define NEUTRAL_MOTORS_DUTYCYCLE    0.076f
-#define MIN_MOTORS_DUTYCYCLE        0.052f
+#define MAX_MOTORS_DUTYCYCLE        0.1f            // Max forward speed duty cycle
+#define NEUTRAL_MOTORS_DUTYCYCLE    0.076f          // Max backward speed duty cycle
+#define MIN_MOTORS_DUTYCYCLE        0.052f          // Stop duty cycle
 
-#define PWM_OUT_RIGHT_MOTOR         PWM_OUT_6
-#define PWM_OUT_LEFT_MOTOR          PWM_OUT_7
+#define PWM_OUT_RIGHT_MOTOR         PWM_OUT_6       // Right wheel PWM output used in some PWM api functions
+#define PWM_OUT_LEFT_MOTOR          PWM_OUT_7       // Left wheel PWM output used in some PWM api functions
 
 #endif /* CONFIG_H_ */
